92b68f73639bc5c6a14fc6bbadcd44fa650fff17
Jesse Lang <jesse@jesselang.com>
2020-11-26 20:04:13 -0600

    Remove uppercase letters from random generator

--- internal/state/rand.go
+++ internal/state/rand.go
@@ -27,11 +27,10 @@ import (
 	"time"
 )
 
-const charset = "abcdefghijklmnopqrstuvwxyz" +
-	"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+// uppercase letters omitted for case-insensitive filesystems
+const charset = "abcdefghijklmnopqrstuvwxyz0123456789"
 
-var seededRand *rand.Rand = rand.New(
-	rand.NewSource(time.Now().UnixNano()))
+var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 func stringWithCharset(length int, charset string) string {
 	b := make([]byte, length)
